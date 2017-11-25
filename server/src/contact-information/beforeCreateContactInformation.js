b13062c822a42716c7a8a3d532748f4be89533c5
Max Davidov <davidovmk@yandex.ru>
2017-11-25 15:31:17 +0300

    add client order

--- server/src/contact-information/beforeCreateContactInformation.js
+++ server/src/contact-information/beforeCreateContactInformation.js
@@ -4,24 +4,25 @@ export default event => {
   const LOCALE = 'ru-RU'
   const { type } = event.data
 
+  /* PREPARE DATA */
   if (type === 'PHONE_NUMBER') {
     event.data.value = event.data.value.replace(/\D/g, '')
   }
-
-  // VALIDATIONS
+  
+  /* VALIDATIONS */
+  if (type === 'PHONE_NUMBER' && !validator.isMobilePhone(event.data.value, LOCALE)) {
+    return {
+      error: 'Phone number is not valid!',
+    }
+  }
+  
   if (type === 'EMAIL' && !validator.isEmail(event.data.value)) {
     return {
       error: 'Email is not valid!',
     }
   }
 
-  if (type === 'PHONE_NUMBER' && !validator.isMobilePhone(event.data.value, LOCALE)) {
-    return {
-      error: 'Phone number is not valid!',
-    }
-  }
-
-  // TRANSFORMATIONS
+  /* TRANSFORMATIONS */
   if (type === 'EMAIL') {
     event.data.value = validator.normalizeEmail(event.data.value)
   }
